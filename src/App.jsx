6c685fd8a04d4e90c61377549c0f936c72bf9b4d
mariaaleacosta <maria.acosta@mercadolibre.com.co>
2022-01-04 16:37:04 -0500

    primeros pasos citas-app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header"
+function App() {
+
+
+
+  return (
+    <div className="App">
+    <>
+    <Header/>
+
+
+    </>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>hola desde el header</h1>
+        </div>
+    );
+}
+
+export default Header;
